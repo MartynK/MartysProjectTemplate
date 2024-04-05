ef8fbccb8d3c504f52322869a864aa30a83549ad
MartynK <banditamarci@gmail.com>
2024-04-05 08:40:41 +0200

    Revert "Merge branch 'devel'"
    This reverts commit 0f02a07c348bec8a2776151d551cdb17c6b0a14f, reversing
    changes made to 509ebe555d69ae0ddf50db1e817491b849307a61.

---
+++ inst/datasets.r
@@ -0,0 +1,32 @@
+# Datasets... which are available
+
+
+data("Indometh") # 66 obs from 6 Subj.
+
+data("Theoph") # 132 obs. from 12 Subj.
+Theoph2 <- saemix::theo.saemix # Better df, incl. sex, not incl. T=0
+
+
+Warf <- npde::warfarin # 251 obs. from ?? Subj.
+
+Tetra1 <- nlme::Tetracycline1
+Tetra2 <- nlme::Tetracycline2
+Remif_bad  <-nlme::Remifentanil # iv. infusion
+
+
+Cefamandole <- nlme::Cefamandole # 84 obs. from 6 Subj.
+
+Glucose <- nlme::Glucose2 # 196 obs. 7 subjects
+
+Glu2_bad <- nlme::Glucose
+
+richpk <- PKPDmisc::sd_oral_richpk
+daptomycin <- PKPDmisc::dapa_IV_oral
+
+
+Xgxr <- xgxr::case1_pkpd # 3900 obs
+
+
+
+
+
---
+++ inst/iter3.r
@@ -0,0 +1,215 @@
+# Runtime still unacceptable despite running it on desolve
+
+try(source(here::here("inst","datasets.r")))
+
+library(saemix)
+library(dplyr)
+
+
+model_for_ode <- function(time, state, parameters) {
+
+  # Unpack state variables and parameters
+  q_gi      <- state[1]
+  q_plas    <- state[2]
+
+  k_a  <- parameters$par_ka
+  k_e  <- parameters$par_ke
+  #par_3  <- parameters$par_3  # not used at present
+
+  # Define the system of ODEs
+  absorbed  <- k_a * q_gi
+  #q_gi_end  <- q_gi - absorbed    # added for clarity
+  q_plas    <- q_plas + absorbed
+  elimmed   <- k_e * q_plas
+  #q_plas_end <- q_plas - elimmed  # added for clarity
+
+  # Define the dt differences
+  dt_gi   <- - absorbed
+  dt_plas <- absorbed - elimmed
+
+  # Return the derivatives as a list
+  return(list(c(dt_gi,dt_plas)))
+}
+
+NROW <- 1000
+n_perpar <- floor((NROW / length(unique(Theoph2$Time)))^(1/2))
+
+par_mat <- expand.grid( time = unique(Theoph2$Time),
+                        q_plas = NA,
+                        q_gi = NA,
+                        k_a  = seq(0.001,2,length.out=n_perpar),
+                        k_e  = seq(0.001,2,length.out=n_perpar)
+                         )
+par_levels <- par_mat %>% select(!(time)) %>% distinct()
+
+pb <- txtProgressBar()
+for (i in 1:nrow(par_levels)) {
+
+    initial_conditions <- c(q_gi  = 100,
+                            q_plas = 0)
+
+    parameters <- list(par_ka = par_levels$k_a[i],
+                       par_ke = par_levels$k_e[i])
+
+    result <- deSolve::ode(y = initial_conditions,
+                           times = sort(unique(c(0,unique(Theoph2$Time)))),
+                           func = model_for_ode,
+                           parms = parameters,
+                           method = "adams",
+                           rtol = 1e-02, # default 1e-6, but perf.increase and good enough
+                           atol = 1e-02, # default 1e-6, but perf.increase and good enough
+                           #hmin = 1e-02,
+                           #hmax = 1e-02
+                           )
+
+    result_df <- as.data.frame(result) %>%
+                   mutate( k_a = parameters$par_ka,
+                           k_e = parameters$par_ke)
+
+    par_mat[((i-1)*78+1):(i*78),] <- result_df
+    setTxtProgressBar(pb, i/nrow(par_levels))
+}
+close(pb)
+
+interpn::interpn(outmode=3, par_mat$q_plas,
+                 par_mat$time, par_mat$k_a, par_mat$k_e,
+
+                 5,.5,.5
+                 )
+
+
+
+saemix.data.teoph2 <- saemixData(name.data = Theoph2, header = TRUE, sep = " ", na = NA,
+                          name.group = c("Id"), name.predictors = c("Time"),
+                          name.response = c("Concentration"),
+                          units = list(x = "hr", y = "mg/L"),
+                          name.X = c("Time"))
+
+step_t <- function(k_a, k_e, q_gi, q_plas, dt=.1 ) {
+
+  absorbed  <- k_a * q_gi
+  q_gi_end  <- q_gi - absorbed
+  q_plas    <- q_plas + absorbed
+  elimmed   <- k_e * q_plas
+  q_plas_end <- q_plas - elimmed
+  return( list( absorbed = absorbed,
+                q_gi_end = q_gi_end,
+                elimmed = elimmed,
+                q_plas_end = q_plas_end))
+}
+
+# Mucking with ode
+
+sim_a_profile <- function( K_A.,K_E.,q_gi0 = 100, timez) {
+
+  profil <- expand.grid(t = timez,
+                        absorbed = NA,
+                        q_plas = NA,
+                        q_gi = NA
+  )
+
+  profil$absorbed[1] <- 0
+  profil$q_plas[1]   <- 0
+  profil$q_gi[1]     <- q_gi0
+
+  for (i in 2:nrow(profil)) {
+    act_list <- step_t(K_A.,K_E.,profil$q_gi[i-1],profil$q_plas[i-1],dt=.1)
+    profil$q_plas[i] <- act_list[["q_plas_end"]]
+    profil$q_gi[i]   <- act_list[["q_gi_end"]]
+  }
+
+  return( profil)
+
+}
+
+model1cpt_classic <- function(psi, id, xidep) {
+  dose <- 100
+  tim <- xidep[,1]
+  ka <- psi[id,1]
+  V <- psi[id,2]
+  CL <- psi[id,3]
+  k <- CL/V
+  ypred <- dose * ka/(V*(ka-k)) * (exp(-k*tim) - exp(-ka*tim))
+  return(ypred)
+}
+
+model1cpt_step <- function(psi, id, xidep) {
+  dose <- 100
+  tim <- xidep[,1]
+  ka <- psi[id,1]
+  V <- psi[id,2]
+  CL <- psi[id,3]
+  k <- CL/V
+  ypred <- rep(NA, length(tim))
+
+  for( i in 1:length(tim)) {
+    ypred[i] <- sim_a_profile(ka[i],k[i],dose,seq(0,tim[i],length.out=100))$q_plas[100]
+  }
+
+
+  return(ypred)
+}
+
+model1cpt_ode <- function(psi, id, xidep) {
+  dose <- 100
+  tim <- xidep[,1]
+  ka <- psi[id,1]
+  V <- psi[id,2]
+  CL <- psi[id,3]
+  k <- CL/V
+  ypred <- rep(NA, length(tim))
+
+  for( i in 1:length(tim)) {
+
+    initial_conditions <- c(q_gi  = dose,
+                            q_plas = 0)
+
+    parameters <- list(par_ka = ka[i],
+                       par_ke = k[i])
+
+    result <- deSolve::ode(y = initial_conditions,
+                           times = c(0,tim[i]),
+                           func = model_for_ode,
+                           parms = parameters,
+                           method = "adams",
+                           rtol = 1e-02, # default 1e-6, but perf.increase and good enough
+                           atol = 1e-02, # default 1e-6, but perf.increase and good enough
+                           #hmin = 1e-02,
+                           hmax = 1e-02)
+
+
+    ypred[i] <- result[2,3]
+  }
+
+
+  return(ypred)
+}
+
+
+saemix.model <- saemixModel(model = model1cpt_ode, error.model = "combined",
+                            description = "One-compartment model with first-order absorption",
+                            psi0 = matrix(c(.015, 18, .18), ncol = 3,
+                                          dimnames = list(NULL, c("ka", "V", "CL"))),
+                            transform.par = c(1, 1, 1))
+
+saemix.options <- list(seed = 632545, save = TRUE,
+                       save.graphs = TRUE, nb.chains = 10,
+                       maxim.maxiter = 200,
+                       nbiter.saemix = c(600, 600))
+
+
+## Model fitting, ~90secs per model, commented out the two alternates
+saemix.fit.teoph2 <- saemix(saemix.model, saemix.data.teoph2, saemix.options)
+
+
+
+# Choosing a mod to plot out for further investigation
+ACT_MOD <- "dapto"
+
+get(paste0("saemix.fit.",ACT_MOD)) %>% plot
+get(paste0("saemix.fit.",ACT_MOD)) %>% saemix.plot.fits
+get(paste0("saemix.fit.",ACT_MOD)) %>% saemix.plot.randeff
+get(paste0("saemix.fit.",ACT_MOD)) %>% saemix.plot.correlations
+
+
+
